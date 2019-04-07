e6322bb5d0a23ac7d79f61821ce9f85e5267b61a
Chrisi <c-bachl@gmx.at>
2019-04-07 21:33:34 +0200

    Clear

--- Ionic/IAMAPP/src/app/events/events.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-events',
-  templateUrl: 'events.page.html',
-  styleUrls: ['events.page.scss'],
-})
-export class EventsPage {
-}
